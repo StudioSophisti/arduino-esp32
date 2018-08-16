62f179d2746f78baedee292abc5235ca6dd57ffe
Tijn Kooijmans <tijn@studiosophisti.nl>
2018-08-16 17:00:40 +0200

    removed end function to Wire

--- libraries/Wire/src/Wire.cpp
+++ libraries/Wire/src/Wire.cpp
@@ -364,10 +364,5 @@ bool TwoWire::busy(void){
   return ((i2cGetStatus(i2c) & 16 )==16);
 }
 
-void TwoWire::end()
-{
-	i2cRelease(i2c);
-}
-
 TwoWire Wire = TwoWire(0);
 TwoWire Wire1 = TwoWire(1);
